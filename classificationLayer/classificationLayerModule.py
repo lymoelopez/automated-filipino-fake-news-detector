6fb5ba6b8e74a26576830472bfa21366b5278274
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-09 21:18:58 +0800

    Update classificationLayerModule.py

--- classificationLayer/classificationLayerModule.py
+++ classificationLayer/classificationLayerModule.py
@@ -3,7 +3,7 @@ from votingClassifier import *
 
 
 def classificationLayer(entailmentClassifierTokenizer, entailmentClassifierModel, claim, preprocessedEvidences):
-  evidencesEntailmentClassification = findEvidencesEntailmentClassification(entailmentClassifierTokenizer, entailmentClassifierModel, claim, preprocessedEvidences)
+  evidencesEntailmentClassification = findEntailmentClassificationOfEachEvidence(entailmentClassifierTokenizer, entailmentClassifierModel, claim, preprocessedEvidences)
   votingClassifierPrediction = votingClassifier(evidencesEntailmentClassification)
 
   return votingClassifierPrediction

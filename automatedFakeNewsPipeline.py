dd854a08b7f09f3ae069a1285d7782722ead7a25
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-09 20:31:30 +0800

    Update automatedFakeNewsPipeline.py

--- automatedFakeNewsPipeline.py
+++ automatedFakeNewsPipeline.py
@@ -7,13 +7,13 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 def automatedFakeNewsPipeline(inputClaim):
 
-  cosineSimilarityModel = SentenceTransformer("transformerModels/danjohnvelasco/filipino-sentence-roberta-v1")
+  cosineSimilarityModel = SentenceTransformer("./transformerModels/danjohnvelasco/filipino-sentence-roberta-v1")
   filteredUrlList, extractedArticlesList, topFiveEvidences = evidenceCollector(inputClaim, cosineSimilarityModel)
 
-  preprocessingTokenizer = AutoTokenizer.from_pretrained("transformerModels/danjohnvelasco/filipino-sentence-roberta-v1")
+  preprocessingTokenizer = AutoTokenizer.from_pretrained("./transformerModels/danjohnvelasco/filipino-sentence-roberta-v1")
   preprocessedEvidences = findPreprocessedEvidences(topFiveEvidences, preprocessingTokenizer)
 
-  transformerModel = 'transformerModels/jcblaise/electra-tagalog-small-uncased-discriminator-newsphnli'
+  transformerModel = './transformerModels/jcblaise/electra-tagalog-small-uncased-discriminator-newsphnli'
   entailmentClassifierTokenizer = AutoTokenizer.from_pretrained(transformerModel)
   entailmentClassifierModel = AutoModelForSequenceClassification.from_pretrained(transformerModel)
 

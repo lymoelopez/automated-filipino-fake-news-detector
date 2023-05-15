44db95070908a8d580f2485ab29470de15fe369f
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-16 01:13:31 +0800

    Update evidenceCollectorModule.py

--- evidenceCollector/evidenceCollectorModule.py
+++ evidenceCollector/evidenceCollectorModule.py
@@ -6,7 +6,12 @@ from findTopEvidences import *
 def evidenceCollector(inputClaim, model, numOfSearchResults=20):
 
   filteredUrlList = webSearcher(inputClaim, numOfSearchResults)
-  extractedArticlesList = createExtractedArticlesList(filteredUrlList)
-  topEvidences, topEvidencesUrl = findTopEvidences(inputClaim, extractedArticlesList, filteredUrlList,model)
+
+  if len(filteredUrlList) == 0:
+    topEvidences = []
+    topEvidencesUrl = []
+  else:
+    extractedArticlesList = createExtractedArticlesList(filteredUrlList)
+    topEvidences, topEvidencesUrl = findTopEvidences(inputClaim, extractedArticlesList, filteredUrlList,model)
 
   return topEvidences, topEvidencesUrl

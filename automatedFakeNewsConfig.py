a34e4c54826717e3c3a23552e3d486e98e458f77
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-15 22:57:52 +0800

    Create automatedFakeNewsConfig.py

---
+++ automatedFakeNewsConfig.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer
+
+def automatedFakeNewsConfig():
+  numOfSearchResults = 20
+  cosineSimilarityModel = SentenceTransformer("danjohnvelasco/filipino-sentence-roberta-v1")
+  preprocessingTokenizer = AutoTokenizer.from_pretrained("danjohnvelasco/filipino-sentence-roberta-v1")
+  transformerModel = 'jcblaise/electra-tagalog-small-uncased-discriminator-newsphnli'
+
+  return numOfSearchResults, cosineSimilarityModel, preprocessingTokenizer, transformerModel

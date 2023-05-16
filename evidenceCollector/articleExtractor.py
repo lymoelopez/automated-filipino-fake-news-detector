318a38a114e39a9d16cbd9f3b0a62ac8457f8075
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-16 10:47:06 +0800

    Update articleExtractor.py
    added exception handling for HTTP error 429

--- evidenceCollector/articleExtractor.py
+++ evidenceCollector/articleExtractor.py
@@ -1,5 +1,7 @@
 from newspaper import Article
 from newspaper import Config
+import requests
+import time
 
 
 def articleExtractor(url, articleExtractorConfig):
@@ -8,6 +10,16 @@ def articleExtractor(url, articleExtractorConfig):
     article = Article(url, config=articleExtractorConfig)
     article.download()
     article.parse()
+
+  except requests.exceptions.HTTError as error:
+
+    if error.response.status_code == 429:
+      print("Too many requests. Waiting for a while before trying again . . . ")
+      time.sleep(5)
+      return articleExtractor(url, articleExtractorConfig)
+    else:
+      raise error
+
   except:
     return ""
 

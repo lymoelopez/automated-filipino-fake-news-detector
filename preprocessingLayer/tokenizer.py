fbf8062edd09208bc5eb3a7cd503381b5c4d0f4a
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-05-23 00:03:01 +0800

    Update tokenizer.py

--- preprocessingLayer/tokenizer.py
+++ preprocessingLayer/tokenizer.py
@@ -1,7 +1,10 @@
 from transformers import AutoTokenizer
 
-def findBPE (text, tokenizer):
-  tokenIDs = tokenizer.encode(text)
-  BPE = tokenizer.convert_ids_to_tokens(tokenIDs)
-  text = " ".join(BPE)
-  return text
+
+def findBPE (text, preprocessingTokenizer):
+
+  BPEtokenIDs =  preprocessingTokenizer.encode(text)
+  BPEtokens =  preprocessingTokenizer.convert_ids_to_tokens(BPEtokenIDs)
+  tokenizedText = " ".join(BPEtokens)
+
+  return tokenizedText

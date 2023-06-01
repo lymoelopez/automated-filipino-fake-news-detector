8a18759bf0b0abdee31a9cc5b983e27ed618a78a
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-06-01 19:30:14 +0800

    Update automatedFakeNewsPipeline.py

--- automatedFakeNewsPipeline.py
+++ automatedFakeNewsPipeline.py
@@ -1,22 +1,19 @@
 from evidenceCollector.evidenceCollectorModule import evidenceCollector
-from preprocessingLayer.findPreprocessedEvidences import findPreprocessedEvidences
+from preprocessingLayer.preprocessingLayerModule import preprocessingLayer
 from classificationLayer.classificationLayerModule import classificationLayer
 from automatedFakeNewsConfig import automatedFakeNewsConfig
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+def automatedFakeNewsPipeline(inputClaim, config=automatedFakeNewsConfig()):
 
-def automatedFakeNewsPipeline(inputClaim):
-
-  urlBanList, cosineSimilarityModel, preprocessingTokenizer, transformerModel = automatedFakeNewsConfig()
-  entailmentClassifierTokenizer = AutoTokenizer.from_pretrained(transformerModel)
-  entailmentClassifierModel = AutoModelForSequenceClassification.from_pretrained(transformerModel)
-
+  inputClaim = inputClaim.lower()
+  urlBanList, cosineSimilarityModel, llm, llmWithPromptTemplate = config
   topEvidences, topEvidencesUrl = evidenceCollector(inputClaim, urlBanList, cosineSimilarityModel)
   
   if len(topEvidencesUrl) == 0:
     finalPrediction = 1
   else:
-    preprocessedEvidences = findPreprocessedEvidences(topEvidences, preprocessingTokenizer)
-    finalPrediction = classificationLayer(entailmentClassifierTokenizer, entailmentClassifierModel, inputClaim, preprocessedEvidences)
+    preprocessedEvidences = preprocessingLayer(topEvidences)
+    finalPrediction = classificationLayer(inputClaim, preprocessedEvidences, llmWithPromptTemplate)
 
   return finalPrediction, topEvidencesUrl

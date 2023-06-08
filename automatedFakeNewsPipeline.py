32629df158f308d005bb222073e148ec19e720c2
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-06-08 21:21:31 +0800

    Update automatedFakeNewsPipeline.py

--- automatedFakeNewsPipeline.py
+++ automatedFakeNewsPipeline.py
@@ -4,16 +4,19 @@ from classificationLayer.classificationLayerModule import classificationLayer
 from automatedFakeNewsConfig import automatedFakeNewsConfig
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
+
 def automatedFakeNewsPipeline(inputClaim, config=automatedFakeNewsConfig()):
 
   inputClaim = inputClaim.lower()
   urlBanList, cosineSimilarityModel, llm, llmWithPromptTemplate = config
-  topEvidences, topEvidencesUrl = evidenceCollector(inputClaim, urlBanList, cosineSimilarityModel)
-  
-  if len(topEvidencesUrl) == 0:
+  topEvidences, highestSimilarityScores = evidenceCollector(inputClaim, urlBanList, cosineSimilarityModel)
+  topEvidencesContent = topEvidences[0]
+  topEvidencesDetails = topEvidences[1:]
+
+  if len(topEvidencesContent) == 0:
     finalPrediction = 1
   else:
-    preprocessedEvidences = preprocessingLayer(topEvidences)
-    finalPrediction = classificationLayer(inputClaim, preprocessedEvidences, llmWithPromptTemplate)
+    preprocessedClaim, preprocessedEvidences = preprocessingLayer(inputClaim, topEvidencesContent)
+    finalPrediction = classificationLayer(preprocessedClaim, preprocessedEvidences, highestSimilarityScores, llmWithPromptTemplate)
 
-  return finalPrediction, topEvidencesUrl
+  return finalPrediction, topEvidencesDetails 

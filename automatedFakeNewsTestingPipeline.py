2a79f5eb7abe1167888e3ea6c08bab6947dafb5e
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-06-10 16:52:15 +0800

    Create automatedFakeNewsTestingPipeline.py

---
+++ automatedFakeNewsTestingPipeline.py
@@ -0,0 +1,25 @@
+from evidenceCollector.evidenceSelector import evidenceSelector
+from preprocessingLayer.findPreprocessedEvidences import findPreprocessedEvidences
+from classificationLayer.classificationLayerModule import classificationLayer
+from automatedFakeNewsConfig import automatedFakeNewsConfig
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+def automatedFakeNewsTestingPipeline(inputClaim, filteredSearchResults, config=automatedFakeNewsConfig(currentDate="June 09 2023")):
+
+  if len(filteredSearchResults[0]) == 0:
+    finalPrediction = 1
+    topEvidencesUrl = []
+    
+  else:
+    inputClaim = inputClaim.lower()
+    urlBanList, cosineSimilarityModel, llm, llmWithPromptTemplate = config
+
+    topEvidences, highestSimilarityScores = evidenceSelector(inputClaim, filteredSearchResults, cosineSimilarityModel)
+    topEvidencesContent = topEvidences[0]
+    topEvidencesUrl = topEvidences[1]
+
+    preprocessedClaim, preprocessedEvidences = preprocessingLayer(inputClaim, topEvidencesContent)
+    finalPrediction = classificationLayer(preprocessedClaim, preprocessedEvidences, highestSimilarityScores, llmWithPromptTemplate)
+
+  return finalPrediction, topEvidencesUrl

3d9cbab7e6621270c992a4ec722d989d5fcf45c6
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-06-11 02:31:50 +0800

    Update evidenceCollectorModule.py

--- evidenceCollector/evidenceCollectorModule.py
+++ evidenceCollector/evidenceCollectorModule.py
@@ -2,19 +2,10 @@ from webSearcher import webSearcher
 from evidenceSelector import evidenceSelector
 
 
-def duckDuckGoBangsRemover(searchQuery):
-  reversedQuery = searchQuery[::-1]
-  newQuery = reversedQuery.replace("!", "!\\",1)[::-1]
-  return newQuery
-
 def evidenceCollector(inputClaim, urlBanList, cosineSimilarityModel):
-
-  try:
-    filteredSearchResults = webSearcher(inputClaim, urlBanList)
-  except AssertionError:
-    inputClaim = duckDuckGoBangsRemover(inputClaim)
-    filteredSearchResults = webSearcher(inputClaim, urlBanList)
   
+  filteredSearchResults = webSearcher(inputClaim, urlBanList)
+
   if len(filteredSearchResults[0]) == 0:
     topEvidences = [[],[],[],[]]
     highestSimilarityScores = []

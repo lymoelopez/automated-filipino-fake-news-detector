d405dc241a811dfc6f57daf2c230cdec9da26300
lymoelopez <lymoel.lopez@eee.upd.edu.ph>
2023-06-19 22:09:22 +0800

    Update classificationLayerModule.py

--- classificationLayer/classificationLayerModule.py
+++ classificationLayer/classificationLayerModule.py
@@ -5,7 +5,7 @@ from votingClassifier import votingClassifier
 def classificationLayer(inputClaim, preprocessedEvidences, highestSimilarityScores, entailmentClassifier):
 
   numberOfEvidences = len(preprocessedEvidences)
-  classificationOfEachEvidence = list(map(llmClassifier , [inputClaim]*numberOfEvidences, preprocessedEvidences, [entailmentClassifier]*numberOfEvidences))
+  classificationOfEachEvidence = list(map(entailmentClassifier , [inputClaim]*numberOfEvidences, preprocessedEvidences, [entailmentClassifier]*numberOfEvidences))
   votingClassifierPrediction = votingClassifier(classificationOfEachEvidence)
 
   return votingClassifierPrediction
